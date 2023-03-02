1dc5a2eb583b241dce91555fd9038c78f46d348b
André Morgado <andremorgado@Andres-MacBook-Pro-2.local>
2023-03-02 17:37:16 +0000

    feat(tests): added tests to test QuizStat class
    This pertains to tasks #15 #17 #18
    
    This is part of the stories #1 #11 #12

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/CreateQuizStatsTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/CreateQuizStatsTest.groovy
@@ -4,14 +4,25 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
-import pt.ulisboa.tecnico.socialsoftware.tutor.teachersdashboard.domain.QuizStats
-import pt.ulisboa.tecnico.socialsoftware.tutor.teachersdashboard.domain.TeacherDashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.QuizStats
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuizAnswer
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+
 
 @DataJpaTest
 class CreateQuizStatsTest extends SpockTest {
 
     def teacher
+    def user
     def teacherdashboard
+    def quizstats
+    def newquiz
+    def quizanswer
+
 
     def setup() {
         createExternalCourseAndExecution()
@@ -19,13 +30,67 @@ class CreateQuizStatsTest extends SpockTest {
         teacher = new Teacher(USER_1_NAME, false)
         userRepository.save(teacher)
 
+        user = new Student(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, false, AuthUser.Type.TECNICO)
+        user.addCourse(externalCourseExecution)
+        userRepository.save(user)
+
         teacherdashboard = new TeacherDashboard(externalCourseExecution, teacher)
+        
     }
     
     def "create quiz stats for a course execution"(){
-        then:
-        teacherdashboard.getNumberOfQuizzes() == 0;
-        teacherdashboard.getNumberOfUniqueQuizzesSolved() == 0;
-        teacherdashboard.getAverageQuizzesSolved() == 0;
+        when: "quiz stat is created to a course execution"
+        quizstats = new QuizStats(externalCourseExecution)
+
+        then: "all stats are zero"
+        quizstats.getNumberOfQuizzes() == 0
+        quizstats.getNumberOfUniqueQuizzesSolved() == 0
+        quizstats.getAverageQuizzesSolved() == 0
     }
+
+    def "create quiz and update"(){
+        given: "quiz stat for a course execution"
+        quizstats = new QuizStats(externalCourseExecution)
+
+        when: "a quiz is added"
+        newquiz = new Quiz()
+        externalCourseExecution.addQuiz(newquiz)
+
+        and: "a student solves the quizz"
+        quizanswer = new QuizAnswer(user, newquiz)
+        quizanswer.setCompleted(true)
+
+        and: "quizz stats are updated"
+        quizstats.update();
+
+        then:
+        quizstats.getNumberOfQuizzes() == 1
+        quizstats.getNumberOfUniqueQuizzesSolved() == 1
+        quizstats.getAverageQuizzesSolved() == 1
+    }
+
+    def "get quiz states with teacher dashboard"(){
+        given: "current course execution quizz stats"
+        int cexIndex = teacherdashboard.getQuizStats().size() - 1
+        quizstats = teacherdashboard.getQuizStats().get(cexIndex)
+
+        when: "a quiz is added"
+        newquiz = new Quiz()
+        externalCourseExecution.addQuiz(newquiz)
+
+        and: "a student solves the quizz"
+        quizanswer = new QuizAnswer(user, newquiz)
+        quizanswer.setCompleted(true)
+
+        and: "quizz stats are updated"
+        teacherdashboard.update()
+
+        then:
+        quizstats.getNumberOfQuizzes() == 1
+        quizstats.getNumberOfUniqueQuizzesSolved() == 1
+        quizstats.getAverageQuizzesSolved() == 1
+    }
+        
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
 }

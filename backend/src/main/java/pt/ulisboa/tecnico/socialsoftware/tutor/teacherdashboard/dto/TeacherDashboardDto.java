a04e89cefbb1421af5967b3d3c30ff0b86edda7a
Digz <diogofonseca2002@gmail.com>
2023-03-12 16:41:39 +0000

    Changed TeacherDashboardDto to contain statistics information This is part of #32

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -2,9 +2,18 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
 
+import java.util.ArrayList;
+
+
+
 public class TeacherDashboardDto {
     private Integer id;
     private Integer numberOfStudents;
+
+    private ArrayList<Integer> numMore75CorrectQuestions;
+    private ArrayList<Integer> numAtLeast3Quizes;
+    private ArrayList<Integer> numStudents;
+
 
     public TeacherDashboardDto() {
     }
@@ -13,6 +22,21 @@ public class TeacherDashboardDto {
         this.id = teacherDashboard.getId();
         // For the number of students, we consider only active students
         this.numberOfStudents = teacherDashboard.getCourseExecution().getNumberOfActiveStudents();
+
+
+        this.numStudents = new ArrayList<>();
+        this.numMore75CorrectQuestions = new ArrayList<>();
+        this.numAtLeast3Quizes = new ArrayList<>();
+
+        teacherDashboard.getStudentStats().forEach(studentStat -> {
+
+            this.numAtLeast3Quizes.add(studentStat.getNumAtLeast3Quizzes());
+            this.numMore75CorrectQuestions.add(studentStat.getNumMore75CorrectQuestions());
+            this.numStudents.add(studentStat.getNumStudents());
+
+        });
+
+
     }
 
     public Integer getId() {
@@ -29,6 +53,18 @@ public class TeacherDashboardDto {
 
     public void setNumberOfStudents(Integer numberOfStudents) {
         this.numberOfStudents = numberOfStudents;
+    }
+
+    public ArrayList<Integer> getNumStudents(){
+        return numStudents;
+    }
+
+    public ArrayList<Integer> getnumMore75CorrectQuestions(){
+        return numMore75CorrectQuestions;
+    }
+
+    public ArrayList<Integer> getnumAtLeast3Quizes(){
+        return numAtLeast3Quizes;
     }
 
     @Override

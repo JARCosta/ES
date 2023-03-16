f3d5bd1f27bb22417286a0a69ab8226fdd935ce3
bernycnunes <bernardo.curralo.nunes@tecnico.ulisboa.pt>
2023-03-16 18:28:27 +0000

    Implementation of new tests

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/RemoveTeacherDashboardTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/RemoveTeacherDashboardTest.groovy
@@ -1,5 +1,4 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.service
-
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
@@ -9,63 +8,65 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
 import spock.lang.Unroll
-
 @DataJpaTest
 class RemoveTeacherDashboardTest extends SpockTest {
-
     def teacher
-
     def setup() {
         createExternalCourseAndExecution()
-
         teacher = new Teacher(USER_1_NAME, false)
         userRepository.save(teacher)
     }
-
     def createTeacherDashboard() {
         def dashboard = new TeacherDashboard(externalCourseExecution, teacher)
         teacherDashboardRepository.save(dashboard)
         return dashboard
     }
-
     def "remove a dashboard"() {
         given: "a dashboard"
         def dashboard = createTeacherDashboard()
-
         when: "the user removes the dashboard"
         teacherDashboardService.removeTeacherDashboard(dashboard.getId())
-
         then: "the dashboard is removed"
         teacherDashboardRepository.findAll().size() == 0L
         teacher.getDashboards().size() == 0
     }
-
     def "cannot remove a dashboard twice"() {
         given: "a removed dashboard"
         def dashboard = createTeacherDashboard()
         teacherDashboardService.removeTeacherDashboard(dashboard.getId())
-
         when: "the dashboard is removed for the second time"
         teacherDashboardService.removeTeacherDashboard(dashboard.getId())
-
-        then: "an exception is thrown"        
+        then: "an exception is thrown"
         def exception = thrown(TutorException)
         exception.getErrorMessage() == ErrorMessage.DASHBOARD_NOT_FOUND
     }
-
     @Unroll
     def "cannot remove a dashboard that doesn't exist with the dashboardId=#dashboardId"() {
         when: "an incorrect dashboard id is removed"
         teacherDashboardService.removeTeacherDashboard(dashboardId)
-
-        then: "an exception is thrown"        
+        then: "an exception is thrown"
         def exception = thrown(TutorException)
         exception.getErrorMessage() == ErrorMessage.DASHBOARD_NOT_FOUND
-
         where:
         dashboardId << [null, 10, -1]
     }
+    def "removes the statistics on teacher dashboard when its removed"(){
+        given: "a dashboard"
+        teacher.addCourse(externalCourseExecution)
+        teacherDashboardService.getTeacherDashboard(externalCourseExecution.getId(),teacher.getId())
+        when: "the user removes the dashboard"
+        assert studentQuestionRepository.count() == 1
+        assert teacherDashboardRepository.count() == 1
+        assert teacher.getDashboards().size() == 1
+        assert teacherDashboardRepository.findAll().get(0).getQuestionStats().size() == 1
+        assert teacherDashboardRepository.findAll().get(0).getTeacher() == teacher
+        teacherDashboardService.removeTeacherDashboard(teacherDashboardRepository.findAll().get(0).getId())
+        then: "the statistics are removed"
+        teacherDashboardRepository.count() == 0
+        teacher.getDashboards().size() == 0
+        studentQuestionRepository.count() == 0
 
+    }
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfiguration {}
 }
--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/UpdateTeacherDashboardTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/UpdateTeacherDashboardTest.groovy
@@ -5,16 +5,67 @@ import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.StudentStats
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
 import spock.lang.Unroll
 
 @DataJpaTest
 class UpdateTeacherDashboardTest extends SpockTest {
+    def teacher
+
     def setup() {
         createExternalCourseAndExecution()
 
-        
+        teacher = new Teacher(USER_1_NAME, false)
+        userRepository.save(teacher)
+        teacher.addCourse(externalCourseExecution)
     }
+
+    def "update a dashboard"() {
+        given: "an empty dashboard for the teacher"
+        def teacherDashboardDto = teacherDashboardService.createTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+        teacherDashboardDto.setNumberOfStudents(100)
+        teacherDashboardDto.setId(99)
+
+        and: "a change on the dashboard's stats"
+        def teacherDashboard = teacherDashboardRepository.findAll().get(0)
+
+        when: "a dashboard is updated"
+        def student = new Student(USER_1_NAME, false)
+        userRepository.save(student)
+        externalCourseExecution.addUser(student)
+        teacherDashboardService.updateTeacherDashboard(teacherDashboard.getId())
+        teacherDashboardDto = teacherDashboardService.getTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+
+        then: "the dashboard is updated for the true values"
+        teacherDashboardRepository.count() == 1L
+        def result = teacherDashboardRepository.findAll().get(0)
+        result.getStudentStats().get(0).getNumStudents() == 1
+        result.getStudentStats().get(0).getNumMore75CorrectQuestions() == 0
+        result.getStudentStats().get(0).getNumAtLeast3Quizzes() == 0
+        teacherDashboard.getStudentStats().size() == 1
+        teacherDashboardDto.getId() == 1
+        teacherDashboardDto.getNumberOfStudents() == 0
+        teacherDashboardDto.getNumStudents() == [1,]
+        teacherDashboardDto.getnumMore75CorrectQuestions() == [0,]
+        teacherDashboardDto.getnumAtLeast3Quizes() == [0,]
+    }
+
+    @Unroll
+    def "cannot update a dashboard that doesn't exist with the dashboardId=#dashboardId"(){
+        when: "an incorrect dashboard id is update"
+        teacherDashboardService.updateTeacherDashboard(dashboardId)
+        then: "an exception is thrown"
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.DASHBOARD_NOT_FOUND
+        where:
+        dashboardId << [null, 10, -1]
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+
 }

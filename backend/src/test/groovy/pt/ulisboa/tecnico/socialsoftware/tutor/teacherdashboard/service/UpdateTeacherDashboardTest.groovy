7ea2029463afbfe2bc332ad68a34af05c79e44a8
bernycnunes <bernardo.curralo.nunes@tecnico.ulisboa.pt>
2023-03-17 14:17:44 +0000

    Tests referring to the Update All

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/UpdateTeacherDashboardTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/UpdateTeacherDashboardTest.groovy
@@ -28,7 +28,7 @@ class UpdateTeacherDashboardTest extends SpockTest {
     }
 
     def createQuestion(available=true) {
-        def question = new Question()
+        def question = new Question() as Object
         question.setTitle("Question Title")
         question.setCourse(externalCourse)
         if (available == true){
@@ -37,7 +37,7 @@ class UpdateTeacherDashboardTest extends SpockTest {
         else{
             question.setStatus(Question.Status.SUBMITTED)
         }
-        def questionDetails = new MultipleChoiceQuestion()
+        def questionDetails = new MultipleChoiceQuestion() as Object
         questionDetails.setQuestion(question)
         question.setQuestionDetails(questionDetails)
         questionRepository.save(question)
@@ -59,7 +59,7 @@ class UpdateTeacherDashboardTest extends SpockTest {
         questionDetails.addOption(optionKO)
         optionRepository.save(optionKO)
 
-        return question;
+        return question
     }
 
     def "update a dashboard"() {
@@ -72,7 +72,7 @@ class UpdateTeacherDashboardTest extends SpockTest {
         def teacherDashboard = teacherDashboardRepository.findAll().get(0)
 
         when: "a dashboard is updated"
-        def student = new Student(USER_1_NAME, false)
+        def student = new Student(USER_1_NAME, false) as Object
         userRepository.save(student)
         externalCourseExecution.addUser(student)
         teacherDashboardService.updateTeacherDashboard(teacherDashboard.getId())
@@ -93,65 +93,114 @@ class UpdateTeacherDashboardTest extends SpockTest {
     }
 
     def "update all teacher dashboard"(){
+        given: "an empty dashboard for the teacher"
+        def teacherDashboardDto = teacherDashboardService.createTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+        teacherDashboardDto.setNumberOfStudents(100)
+        teacherDashboardDto.setId(99)
+
+        and: "a change on the dashboard's stats"
+        def teacherDashboard = teacherDashboardRepository.findAll().get(0)
+
+        when: "a dashboard is updated"
+        def student = new Student(USER_1_NAME, false) as Object
+        userRepository.save(student)
+        externalCourseExecution.addUser(student)
+        teacherDashboardService.updateTeacherDashboard(teacherDashboard.getId())
+        teacherDashboardDto = teacherDashboardService.getTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+
+        then: "the dashboard is updated for the true values"
+        teacherDashboardRepository.count() == 1L
+        def result = teacherDashboardRepository.findAll().get(0)
+        result.getStudentStats().get(0).getNumStudents() == 1
+        result.getStudentStats().get(0).getNumMore75CorrectQuestions() == 0
+        result.getStudentStats().get(0).getNumAtLeast3Quizzes() == 0
+        teacherDashboard.getStudentStats().size() == 1
+        teacherDashboardDto.getId() == 1
+        teacherDashboardDto.getNumberOfStudents() == 0
+        teacherDashboardDto.getNumStudents() == [1,]
+        teacherDashboardDto.getnumMore75CorrectQuestions() == [0,]
+        teacherDashboardDto.getnumAtLeast3Quizes() == [0,]
+    }
+
+    def "update a dashboard with a quiz"() {
+        given: "an empty dashboard for the teacher"
+        def teacherDashboardDto = teacherDashboardService.createTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+        teacherDashboardDto.setNumberOfStudents(100)
+        teacherDashboardDto.setId(99)
+
+        and: "a change on the dashboard's stats"
+        def teacherDashboard = teacherDashboardRepository.findAll().get(0)
+
+        when: "a dashboard is updated"
+        def student = new Student(USER_1_NAME, false) as Object
+        userRepository.save(student)
+        externalCourseExecution.addUser(student)
+        def quiz = new Quiz(externalCourseExecution) as Object
+        quiz.setStatus(Quiz.Status.AVAILABLE)
+        quiz.setTitle("Quiz 1")
+        quizRepository.save(quiz)
+        def quizQuestion = new QuizQuestion(quiz, createQuestion()) as Object
+        quizQuestion.setSequence()
         given: "three students"
-        def student1 = new Student(USER_1_NAME, false)
+
+        def student1 = new Student(USER_1_NAME, false) as Object
         userRepository.save(student1)
         externalCourseExecution.addUser(student1)
 
-        def student2 = new Student(USER_2_NAME, false)
+        def student2 = new Student(USER_2_NAME, false) as Object
         userRepository.save(student2)
         externalCourseExecution.addUser(student2)
 
-        def student3 = new Student(USER_3_NAME, false)
+        def student3 = new Student(USER_3_NAME, false) as Object
         userRepository.save(student3)
         externalCourseExecution.addUser(student3)
 
         and: "three quizzes"
-        def quiz1 = new Quiz(externalCourseExecution)
+        def quiz1 = new Quiz(externalCourseExecution) as Object
         quiz1.setStatus(Quiz.Status.AVAILABLE)
         quiz1.setTitle("Quiz 1")
         quiz1Repository.save(quiz1)
 
-        def quiz2 = new Quiz(externalCourseExecution)
+        def quiz2 = new Quiz(externalCourseExecution) as Object
         quiz2.setStatus(Quiz.Status.AVAILABLE)
         quiz2.setTitle("Quiz 2")
         quiz2Repository.save(quiz2)
 
-        def quiz3 = new Quiz(externalCourseExecution)
+        def quiz3 = new Quiz(externalCourseExecution) as Object
         quiz3.setStatus(Quiz.Status.AVAILABLE)
         quiz3.setTitle("Quiz 3")
         quiz3Repository.save(quiz3)
 
         and: "three questions for each quiz"
-        def quizQuestion1 = new QuizQuestion(quiz1, createQuestion())
+        def quizQuestion1 = new QuizQuestion(quiz1, createQuestion()) as Object
         quizQuestion1.setSequence(0)
         quizQuestionRepository.save(quizQuestion1)
 
-        def quizQuestion2 = new QuizQuestion(quiz1, createQuestion())
+        def quizQuestion2 = new QuizQuestion(quiz1, createQuestion()) as Object
         quizQuestion2.setSequence(1)
         quizQuestionRepository.save(quizQuestion2)
 
-        def quizQuestion3 = new QuizQuestion(quiz1, createQuestion())
+        def quizQuestion3 = new QuizQuestion(quiz1, createQuestion()) as Object
         quizQuestion3.setSequence(2)
         quizQuestionRepository.save(quizQuestion3)
 
-        def quizQuestion4 = new QuizQuestion(quiz2, createQuestion())
+        def quizQuestion4 = new QuizQuestion(quiz2, createQuestion()) as Object
         quizQuestion4.setSequence(0)
         quizQuestionRepository.save(quizQuestion4)
 
-        def quizQuestion5 = new QuizQuestion(quiz2, createQuestion())
+        def quizQuestion5 = new QuizQuestion(quiz2, createQuestion()) as Object
         quizQuestion5.setSequence(1)
         quizQuestionRepository.save(quizQuestion5)
 
-        def quizQuestion6 = new QuizQuestion(quiz2, createQuestion())
+        def quizQuestion6 = new QuizQuestion(quiz2, createQuestion()) as Object
         quizQuestion6.setSequence(2)
         quizQuestionRepository.save(quizQuestion6)
 
-        def quizQuestion7 = new QuizQuestion(quiz3, createQuestion())
+        def quizQuestion7 = new QuizQuestion(quiz3, createQuestion()) as Object
         quizQuestion7.setSequence(0)
         quizQuestionRepository.save(quizQuestion7)
 
-        def quizQuestion8 = new QuizQuestion(quiz3, createQuestion())
+        def quizQuestion8 = new QuizQuestion(quiz3, createQuestion()) as Object
         quizQuestion8.setSequence(1)
 
         and: "four questions"

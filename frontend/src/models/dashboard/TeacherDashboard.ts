4aeb776f606aef854ea3024209809fe7438bbe99
João Diegues <jrcarrasqueirodiegues@tecnico.ulisboa.pt>
2023-04-10 16:45:36 +0100

    feat(frontend): gets information from backend
    This pertains to task #55
    
    This is part of story #56

--- frontend/src/models/dashboard/TeacherDashboard.ts
+++ frontend/src/models/dashboard/TeacherDashboard.ts
@@ -1,11 +1,27 @@
 export default class TeacherDashboard {
   id!: number;
-  numberOfStudents!: number;
+  numStudents!: number;
+  numMore75CorrectQuestions!: number;
+  numAtLeast3Quizzes!: number;
+  numQuizzes!: number;
+  numUniqueAnsweredQuizzes!: number;
+  averageQuizzesSolved!: number;
+  numAvailable!: number;
+  answeredQuestionsUnique!: number;
+  averageQuestionsAnswered!: number;
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
       this.id = jsonObj.id;
-      this.numberOfStudents = jsonObj.numberOfStudents;
+      this.numStudents = jsonObj.numStudents;
+      this.numMore75CorrectQuestions = jsonObj.numMore75CorrectQuestions;
+      this.numAtLeast3Quizzes = jsonObj.numAtLeast3Quizzes;
+      this.numQuizzes = jsonObj.numQuizzes;
+      this.numUniqueAnsweredQuizzes = jsonObj.numUniqueAnsweredQuizzes;
+      this.averageQuizzesSolved = jsonObj.averageQuizzesSolved;
+      this.numAvailable = jsonObj.numAvailable;
+      this.answeredQuestionsUnique = jsonObj.answeredQuestionsUnique;
+      this.averageQuestionsAnswered = jsonObj.averageQuestionsAnswered;
     }
   }
 }

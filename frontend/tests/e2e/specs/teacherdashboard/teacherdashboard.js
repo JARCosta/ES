8ecf3d082910b4a6c57c57a815960339863b6318
André Morgado <andre.filipe.morgado@tecnico.ulisboa.pt>
2023-04-14 01:21:14 +0100

    feat(e2e): Adds the cypress test: 2.1 - "Values shown in the dashboard for 2023 are correct and verify if the 3 graphs related to course executions 2023, 2022 e 2019 are shown"; Also this commit implements a useful function that compares pixels in images (very usefull to check if the graphs shown are correct)
    This belongs to task #57
    
    This is part of the story #59

---
+++ frontend/tests/e2e/specs/teacherdashboard/teacherdashboard.js
@@ -0,0 +1,68 @@
+const { compareScreenshots } = require('../../support/utils');
+
+describe('TeacherDashboard', () => {
+    before(() => {
+        cy.cleanDb()
+
+        cy.createCourseExecutionOnDemoCourse("2019/2020");
+        cy.createCourseExecutionOnDemoCourse("2020/2021");
+        cy.createCourseExecutionOnDemoCourse("2021/2022");
+
+        cy.createQuizStats();
+        cy.createStudentStats();
+        cy.createQuestionStats();
+    });
+
+    beforeEach(() => {
+
+        cy.request('http://localhost:8080/auth/demo/teacher')
+        .as('loginResponse')
+        .then((response) => {
+          Cypress.env('token', response.body.token);
+          return response;
+        });
+
+        cy.demoTeacherLogin();
+    });
+    
+    
+
+    after(() => {
+        cy.cleanDb()
+    });
+
+    it('teacher accesses dashboard', () => {
+        cy.intercept('GET', '**/teachers/dashboards/executions/*').as(
+          'getDashboard'
+        );
+        
+        cy.get('[data-cy="dashboardMenuButton"]').click();
+        cy.wait('@getDashboard');
+    });
+    
+    it('check if values in the containers are correct', () => {
+        cy.get('[data-cy="dashboardMenuButton"]').click();
+
+        cy.get('[data-cy="numStudents"]').contains('13');
+        cy.get('[data-cy="numMore75CorrectQuestions"]').contains('13');
+        cy.get('[data-cy="numAtLeast3Quizzes"]').contains('13');
+        cy.get('[data-cy="numQuizzes"]').contains('13');
+        cy.get('[data-cy="numUniqueAnsweredQuizzes"]').contains('13');
+        cy.get('[data-cy="averageQuizzesSolved"]').contains('13');
+        cy.get('[data-cy="numAvaible"]').contains('13');
+        cy.get('[data-cy="answeredQuestionsUnique"]').contains('13');
+        cy.get('[data-cy="averageQuestionsAnswered"]').contains('13');
+    });
+
+    it('check if bar graphs are correct', () => {
+        cy.get('[data-cy="dashboardMenuButton"]').click();
+        
+        cy.get('[data-cy="studentStatsGraph"]').eq(0).scrollIntoView().wait(5000).screenshot('studentStatsGraph2-1')
+        cy.get('[data-cy="quizStatsGraph"]').eq(0).scrollIntoView().wait(5000).screenshot('quizStatsGraph2-1')
+        cy.get('[data-cy="questionStatsGraph"]').eq(0).scrollIntoView().wait(5000).screenshot('questionStatsGraph2-1')
+        
+        compareScreenshots('expected-screenshot/studentStatsGraph2-1.png', 'studentStatsGraph2-1.png', 'diff1');
+        compareScreenshots('expected-screenshot/quizStatsGraph2-1.png', 'quizStatsGraph2-1.png', 'diff2');
+        compareScreenshots('expected-screenshot/questionStatsGraph2-1.png', 'questionStatsGraph2-1.png', 'diff3');
+    });
+});
---
+++ frontend/tests/e2e/support/utils.js
@@ -0,0 +1,29 @@
+const pixelmatch = require('pixelmatch');
+const PNG = require('pngjs').PNG;
+
+function compareScreenshots(name1, name2, diffName) {
+    cy.readFile(`tests/e2e/specs/teacherdashboard/${name1}`, 'base64').then((img1) => {
+    cy.readFile(`tests/e2e/screenshots/teacherdashboard.js/${name2}`, 'base64').then((img2) => {
+      const img1Png = PNG.sync.read(Buffer.from(img1, 'base64'));
+      const img2Png = PNG.sync.read(Buffer.from(img2, 'base64'));
+      const { width, height } = img1Png;
+      const diff = new PNG({ width, height });
+      const numDiffPixels = pixelmatch(
+        img1Png.data,
+        img2Png.data,
+        diff.data,
+        width,
+        height,
+        { threshold: 0.1 }
+      );
+      if (numDiffPixels > 0) {
+        cy.writeFile(`cypress/screenshots/${diffName}.png`, PNG.sync.write(diff), 'binary');
+      }
+      expect(numDiffPixels).to.equal(0);
+    });
+  });
+}
+  
+  module.exports = {
+    compareScreenshots
+  };

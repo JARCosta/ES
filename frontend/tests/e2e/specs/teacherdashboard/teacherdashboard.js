f17fde552435906b17328dbe910bff83e943e552
André Morgado <andre.filipe.morgado@tecnico.ulisboa.pt>
2023-04-14 03:20:43 +0100

    fix(frontend): tries to fix pipeline failure

--- frontend/tests/e2e/specs/teacherdashboard/teacherdashboard.js
+++ frontend/tests/e2e/specs/teacherdashboard/teacherdashboard.js
@@ -2,15 +2,27 @@ const { compareScreenshots } = require('../../support/utils');
 
 describe('TeacherDashboard', () => {
     before(() => {
-        cy.cleanDb()
+        cy.cleanDb();
 
         cy.createCourseExecutionOnDemoCourse("2019/2020");
         cy.createCourseExecutionOnDemoCourse("2020/2021");
         cy.createCourseExecutionOnDemoCourse("2021/2022");
 
+        cy.request('http://localhost:8080/auth/demo/teacher')
+        .as('loginResponse')
+        .then((response) => {
+          Cypress.env('token', response.body.token);
+          return response;
+        });
+
+        cy.demoTeacherLogin();
+        
+        cy.createTeacherDashboard();
+
         cy.createQuizStats();
         cy.createStudentStats();
         cy.createQuestionStats();
+
     });
 
     beforeEach(() => {
@@ -24,12 +36,7 @@ describe('TeacherDashboard', () => {
 
         cy.demoTeacherLogin();
     });
-    
-    
 
-    after(() => {
-        cy.cleanDb()
-    });
 
     it('teacher accesses dashboard', () => {
         cy.intercept('GET', '**/teachers/dashboards/executions/*').as(
@@ -56,7 +63,7 @@ describe('TeacherDashboard', () => {
 
     it('check if bar graphs are correct', () => {
         cy.get('[data-cy="dashboardMenuButton"]').click();
-        
+
         cy.get('[data-cy="studentStatsGraph"]').eq(0).scrollIntoView().wait(5000).screenshot('studentStatsGraph2-1')
         cy.get('[data-cy="quizStatsGraph"]').eq(0).scrollIntoView().wait(5000).screenshot('quizStatsGraph2-1')
         cy.get('[data-cy="questionStatsGraph"]').eq(0).scrollIntoView().wait(5000).screenshot('questionStatsGraph2-1')

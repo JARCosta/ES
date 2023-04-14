0c107cc7bf0cf0c40fc8bf1672276bab951ad5b0
André Morgado <andre.filipe.morgado@tecnico.ulisboa.pt>
2023-04-14 14:11:46 +0100

    fix(frontend): fixes teacherdashboard test
    This pertains to task #59
    
    Belongs to story #57

--- frontend/tests/e2e/support/utils.js
+++ frontend/tests/e2e/support/utils.js
@@ -8,18 +8,18 @@ function compareScreenshots(name1, name2, diffName) {
       const img2Png = PNG.sync.read(Buffer.from(img2, 'base64'));
       const { width, height } = img1Png;
       const diff = new PNG({ width, height });
-      // const numDiffPixels = pixelmatch(
-      //   img1Png.data,
-      //   img2Png.data,
-      //   diff.data,
-      //   width,
-      //   height,
-      //   { threshold: 0.1 }
-      // );
-      // if (numDiffPixels > 0) {
-      //   cy.writeFile(`cypress/screenshots/${diffName}.png`, PNG.sync.write(diff), 'binary');
-      // }
-      // expect(numDiffPixels).to.equal(0);
+      const numDiffPixels = pixelmatch(
+        img1Png.data,
+        img2Png.data,
+        diff.data,
+        width,
+        height,
+        { threshold: 0.1 }
+      );
+      if (numDiffPixels > 0) {
+        cy.writeFile(`cypress/screenshots/${diffName}.png`, PNG.sync.write(diff), 'binary');
+      }
+      expect(numDiffPixels).to.equal(0);
     });
   });
 }
